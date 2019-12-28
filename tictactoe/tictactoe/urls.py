4236fe3be69a50ddcadda8c569612e31de4c5337
satishkumar <satishkumar.dhule@gmail.com>
2019-12-28 18:31:17 +0530

    minor fixes to settings , urls and views

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('player/', include('player.urls')),
+    path('welcome/', welcome),
 ]
