899c0b4c828be2a7b8c92261c0e51594d75aa75e
satishkumar <satishkumar.dhule@gmail.com>
2019-12-28 11:05:49 +0530

    Added list games for a player funcationality

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from gameplay.models import Game
 
 # Create your views here.
 def home(request):
-    return render(request,'player/home.html')
+    list_games=list(Game.objects.filter(first_player=request.user, status='F'))+list(Game.objects.filter(second_player=request.user, status='S'))
+    return render(request,'player/home.html',{'games':list_games})
