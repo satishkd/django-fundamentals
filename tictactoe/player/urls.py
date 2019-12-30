1e72a0bee0640acff57251b6248bc1e1e84337c7
satishkumar <satishkumar.dhule@gmail.com>
2019-12-30 09:23:03 +0530

    added login form and logout view

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import home
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('home/', home, name="player_home"),
+    path('login/', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
+    path('logout/', LogoutView.as_view(), name="player_login"),
 ]
