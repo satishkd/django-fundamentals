2ed96df65d77ad58947eeb0acedb9dddac7e25e2
satishkumar <satishkumar.dhule@gmail.com>
2020-01-01 11:36:24 +0530

    Working login and logout funcationlity

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('home/', home, name="player_home"),
     path('login/', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
-    path('logout/', LogoutView.as_view(), name="player_login"),
+    path('logout/', LogoutView.as_view(), name="player_logout"),
 ]
