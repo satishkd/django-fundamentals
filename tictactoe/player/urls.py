12cacbd20881d1fd0361e1b4058d6bce6ac36b6a
satishkumar <satishkumar.dhule@gmail.com>
2020-01-02 07:34:54 +0530

    Send invitation and accept invitation implemented

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home
-from django.contrib.auth.views import LoginView,LogoutView
+from .views import home, new_invitation, accept_invitation
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls import url
 
 urlpatterns = [
     path('home/', home, name="player_home"),
     path('login/', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
     path('logout/', LogoutView.as_view(), name="player_logout"),
+    url(r'new_invitation$', new_invitation, name="player_new_invitation"),
+    url(r'accept_invitation/(?P<id>\d+)/$', accept_invitation, name="player_accept_invitation"),
 ]
